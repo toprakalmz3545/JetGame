d0ed89025463b5905bbfd932bbccca1079bf2e7b
gamejet <toprakbinomo@gmail.com>
2025-07-15 17:50:09 +0300

    Add files via upload
    Signed-off-by: gamejet <toprakbinomo@gmail.com>

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('loginForm').addEventListener('submit', function(e) {
   e.preventDefault();
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  const username = document.getElementById('username').value.trim();
+  const password = document.getElementById('password').value.trim();
   const errorMessage = document.getElementById('errorMessage');
 
   if (username === 'admin' && password === '1234') {
